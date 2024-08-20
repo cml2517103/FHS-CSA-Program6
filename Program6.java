c2758e7a5696c49140f501df4972d6d57c0481db
cml2517103 <cml2517103@gmail.com>
2024-08-20 09:16:32 -0700

    Update Program6.java

--- Program6.java
+++ Program6.java
@@ -1,9 +1,20 @@
-//import stuff here
+import java.util.Scanner;
 
-//Your code here
-
-//Paste console output below:
-/*
-
-
-*/
+public class Program6{
+    public static void main(String[] args){
+        Scanner survey = new Scanner(System.in);
+        final double PI = 3.14159;
+        
+        System.out.print("Enter the radius: ");
+        double radius = survey.nextDouble();
+        
+        double diameter = radius * 2;
+        double area = PI * (radius * radius);
+        double circumference = PI * diameter;
+        
+        System.out.printf("\tThe Radius of the circle = %.3f", radius); System.out.println();
+        System.out.printf("\tThe DIameter of the circle = %.3f", diameter); System.out.println();
+        System.out.printf("\tThe Area of the circle = %.3f", area); System.out.println();
+        System.out.printf("\tThe Circumference of the circle = %.3f", circumference);
+    }    
+}
